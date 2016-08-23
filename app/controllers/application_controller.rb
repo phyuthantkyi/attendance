3de73b7a3a6a75b3c83758158f542eb019c1c48b
phyuthantkyi <phyuthantkyi.cloudsource@gmail.com>
2016-08-23 13:05:38 +0630

    Register

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,9 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(user)
     new_user_session_path
   end
-
+   
+  rescue_from CanCan::AccessDenied do |exception|
+        flash[:error] = exception.message
+        redirect_to root_url
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'SETTING UP DEFAULT ADMIN LOGIN'
+user = User.create! :email => 'ogawa@cloudsource.co.jp', :password => 'password', :password_confirmation => 'password', :admin => 'true'
+puts 'New admin created: ' << user.email

c6b2f0ce89db0729d3f91e3fad10b01c0c8284c1
phyuthantkyi <phyuthantkyi.cloudsource@gmail.com>
2016-09-08 15:11:07 +0630

    register-modify

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys:[:name, :staff_id, :email, :password, :password_confirmation, :position, :role]) 
-    devise_parameter_sanitizer.permit(:account_update, keys:[:name, :staff_id, :email, :password, :password_confirmation, :position, :role]) 
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:name, :staff_id, :email, :password, :password_confirmation, :position, :role])
+    devise_parameter_sanitizer.permit(:account_update, keys:[:name, :staff_id, :email, :password, :password_confirmation, :position, :role])
   end
 
   def after_sign_in_path_for(user)
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(user)
     new_user_session_path
   end
-   
+
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = exception.message
     redirect_to root_url
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,12 +1,25 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_filter :check_permissions, :only => [:new, :create, :cancel]
+  #before_filter :check_permissions, :only => [:new, :create, :cancel]
   skip_before_filter :require_no_authentication
 
-  def check_permissions
-    authorize! :create, resource
-  end
+
+  # def check_permissions
+  #   authorize! :create, resource
+  # end
 
   def after_update_path_for(user)
     user_home_path
   end
+
+  def create
+    member_params = params[:user].permit(:name, :staff_id, :email, :password, :password_confirmation, :position, :role) if params[:user]
+    @member = User.new(member_params)
+    respond_to do |format|
+      if @member.save
+        format.html { redirect_to user_home_path, notice: 'User are created'}
+      else
+        format.html　{ render :new }
+      end
+    end
+   end
 end
